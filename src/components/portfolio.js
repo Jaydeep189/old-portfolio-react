a121bf9446133744a5dd0effe6c87d12698f4665
Jaydeep Sharma <sjaydeep189@gmail.com>
2022-02-24 15:57:48 +0530

    overkill

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -23,7 +23,7 @@ export default function Portfolio(){
 				<WorkModal img="images/portfolio/ruleout_p.png" modalNum="05" title="Rule Out" alt="Beetle" desc="Clothing" />
 				<WorkModal img="images/portfolio/n.png" modalNum="04" title="Existential to-do List" alt="Lighthouse" desc="Web Dev + Philosophy" />
 				<Modals  img="images/portfolio/modals/zerobitcode_final_1.png" modalNum="01" title="Zerobitcode" alt="zerobitcode" category="Web And App Development Company" desc="We at ZeroBitCode provide the Best Quality websites and apps at affordable prices. Our goal is to digitalize bussniesses around the world." />
-				<Modals  img="images/portfolio/wwh_s.svg" modalNum="02" alt="whywhathow" title="whywhathow" alt="whywhathow" category="Blog" desc="Why What How is a food stall for curious people to feed their curiosity. This is my Blog where I write interesting Stuff!"  />
+				<Modals  img="images/portfolio/wwh_s.svg" modalNum="02" alt="whywhathow" title="whywhathow" category="Blog" desc="Why What How is a food stall for curious people to feed their curiosity. This is my Blog where I write interesting Stuff!"  />
 				<Modals  img="images/portfolio/modals/m-clouds.jpg" modalNum="03" title="Fiction" alt="Clouds" category="Blog" desc= "Here I express my love for art that reflects like a mirror into reality and much more" />
 				<Modals  img="images/portfolio/ruleout_p.png" modalNum="05" title="Rule Out" alt="Beetle" category="Clothing" desc="Here we design clothing that are unique to it's design and make you stand out!" />
 				<Modals  img="images/portfolio/n2.jpg"  modalNum="04" title="Existential to-do List" alt="Lighthouse" category="Web Dev + Philosophy" desc="This is an site where you priotize your work by asking yourself Existential questions!" />
